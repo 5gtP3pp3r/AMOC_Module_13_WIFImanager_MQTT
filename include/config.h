c6a1169dac5e8bfb70a356aa09a7b0f6e74d7cec
5gtp3pp3r <william.crepault@gmail.com>
2025-01-10 15:36:24 -0500

    retrait IP des #defines

--- include/config.h
+++ include/config.h
@@ -2,13 +2,9 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 
-#define PORTAL_SSID "ConfigurationESP32"
+#define PORTAL_SSID "5gtP3pp3rConfigurationESP32"
 #define PORTAL_PASSWWORD "Pouet&666"
 #define TIMEOUT 180
-#define PORTAL_IP (192, 168, 23, 1)
-#define GATEWAY_IP (192, 168, 23, 1)
-#define PORTAL_MASK (255, 255, 255, 0)
-
 #define PARAM_1 "Identifiant_unique_champ"
 #define PARAM_2 "Nom du champ"
 #define PARAM_3 "ValeurQuiEtaitSauvegardee"
