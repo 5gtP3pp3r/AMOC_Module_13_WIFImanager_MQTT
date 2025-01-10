c21206c17a03b0720aca2efac8c785844184be0d
5gtp3pp3r <william.crepault@gmail.com>
2025-01-10 02:08:26 -0500

    Changement et ajout #define dans config.h et const.h

--- include/config.h
+++ include/config.h
@@ -1,3 +1,15 @@
 #pragma once
 #include <Arduino.h>
+#include <WiFiManager.h>
 
+#define PORTAL_SSID "ConfigurationESP32"
+#define PORTAL_PASSWWORD "Pouet&666"
+#define TIMEOUT 180
+#define PORTAL_IP (192, 168, 23, 1)
+#define GATEWAY_IP (192, 168, 23, 1)
+#define PORTAL_MASK (255, 255, 255, 0)
+
+#define PARAM_1 "Identifiant_unique_champ"
+#define PARAM_2 "Nom du champ"
+#define PARAM_3 "ValeurQuiEtaitSauvegardee"
+#define PARAM_4 40
--- include/const.h
+++ include/const.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #define SERIAL_SPEED 115200
+#define BUTTON1_PIN 25
+
+#define DEFAULT_ZERO 0
+#define INTERVAL_40 40
+
+#define JSON_FILE_PATH "/config.json"
