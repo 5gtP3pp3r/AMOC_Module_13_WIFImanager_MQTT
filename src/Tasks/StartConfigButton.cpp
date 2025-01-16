043c5fda36c6cd2ff0fa08618411f7123ef38e14
5gtp3pp3r <william.crepault@gmail.com>
2025-01-16 11:36:34 -0500

    complétion tick() des 2 boutons

--- src/Tasks/StartConfigButton.cpp
+++ src/Tasks/StartConfigButton.cpp
@@ -7,7 +7,6 @@ StartConfigButton::StartConfigButton(
     ) : Button(p_pin, p_actionDelayMS, p_action) { ; }
 
 void StartConfigButton::tick() {
-    
     this->m_buttonState = digitalRead(this->m_pin);
     this->m_actualTime = millis();
     if (this->m_buttonState != this->m_lastButtonState) {
@@ -16,9 +15,6 @@ void StartConfigButton::tick() {
     }
     if (this->m_actualTime - this->m_lastTimeChange >  this->m_actionDelayMS) {
         if (this->m_lastButtonStableState == LOW && this->m_buttonState == HIGH) {
-            Serial.println("BoutonStartConfig");
-            Serial.print("Adresse de m_actionResetWIFIManager : ");
-            Serial.println((uintptr_t)m_action, HEX);
             this->m_action->execute();
         }
         this->m_lastButtonStableState = this->m_buttonState;
